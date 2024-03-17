45ee7a9ffad7b1d4ec19220957d3171906d8a97d
maria parma <158210075+Mariaparma@users.noreply.github.com>
2024-03-16 22:16:19 -0300

    Update array01.js

--- array01.js
+++ array01.js
@@ -8,3 +8,18 @@ console.log(novoArray); // Saída: ['1', '2', '3']
 let novoArray = array.slice('1', '5');
 console.log(novoArray); // Saída: ['1'', '2', '3', '4']
 
+// Método concat()
+//Retorna um novo array que é a concatenação do array 'array1' com o array 'array2'
+let array1 = ['1', '2', '3'];
+let array2 = ['4', '5', '6'];
+let arrayConcatenado = array1.concat(array2);
+console.log(arrayConcatenado); // Saída: ['1', '2', '3', '4', '5', '6']
+
+// Método concat()
+//Retorna um novo array que é a concatenação do array 'array1' com o array 'array2'
+let array1 = ['10', '20', '30'];
+let array2 = ['40', '50', '60'];
+let arrayConcatenado = array1.concat(array2);
+console.log(arrayConcatenado); // Saída: ['10', '20', '30', '40', '50', '60']
+
+
