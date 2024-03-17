2ddbf1861fb5bbf42f181eddffeaeac93a95e987
maria parma <158210075+Mariaparma@users.noreply.github.com>
2024-03-16 21:55:16 -0300

    metodo slice

--- array01.js
+++ array01.js
@@ -1,13 +1,4 @@
-// Método concat()
-//Retorna um novo array que é a concatenação do array 'array1' com o array 'array2'.
-let array1 = ['1', '2', '3'];
-let array2 = ['4', '5', '6'];
-let arrayConcatenado = array1.concat(array2);
-console.log(arrayConcatenado); // Saída: ['1', '2', '3', '4', '5', '6']
-
-// Método concat()
-//Retorna um novo array que é a concatenação do array 'array1' com o array 'array2'.
-let array1 = ['10', '20', '30'];
-let array2 = ['40', '50', '60'];
-let arrayConcatenado = array1.concat(array2);
-console.log(arrayConcatenado); // Saída: ['10', '20', '30', '40', '50', '60']
+// Método slice()
+//Retorna uma cópia de parte do array, selecionada do índice 1 (inclusive) ao índice 4 (exclusive)
+let novoArray = array.slice(1, 4);
+console.log(novoArray); // Saída: [1, 2, 3]
