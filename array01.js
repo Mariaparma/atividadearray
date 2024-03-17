e608c78b0a89cb06b6557c53a30d96ecf2484351
maria parma <158210075+Mariaparma@users.noreply.github.com>
2024-03-16 21:27:38 -0300

    mudança dos numeros

--- array01.js
+++ array01.js
@@ -4,3 +4,10 @@ let array1 = ['1', '2', '3'];
 let array2 = ['4', '5', '6'];
 let arrayConcatenado = array1.concat(array2);
 console.log(arrayConcatenado); // Saída: ['1', '2', '3', '4', '5', '6']
+
+// Método concat()
+//Retorna um novo array que é a concatenação do array 'array1' com o array 'array2'.
+let array1 = ['10', '20', '30'];
+let array2 = ['40', '50', '60'];
+let arrayConcatenado = array1.concat(array2);
+console.log(arrayConcatenado); // Saída: ['10', '20', '30', '40', '50', '60']
