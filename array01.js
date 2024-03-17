245a7c0f310fbc436b047d258f2310d5ccc0e5ab
maria parma <158210075+Mariaparma@users.noreply.github.com>
2024-03-16 22:11:39 -0300

    Update array01.js

--- array01.js
+++ array01.js
@@ -1,4 +1,10 @@
 // Método slice()
 //Retorna uma cópia de parte do array, selecionada do índice 1 (inclusive) ao índice 4 (exclusive)
-let novoArray = array.slice(1, 4);
-console.log(novoArray); // Saída: [1, 2, 3]
+let novoArray = array.slice('1', '4');
+console.log(novoArray); // Saída: ['1', '2', '3']
+
+// Método slice()
+//Retorna uma cópia de parte do array, selecionada do índice 1 (inclusive) ao índice 4 (exclusive)
+let novoArray = array.slice('1', '5');
+console.log(novoArray); // Saída: ['1'', '2', '3', '4']
+
