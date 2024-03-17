14d1f3f1d72731ad32321d4511f4fa589f5870a3
maria parma <158210075+Mariaparma@users.noreply.github.com>
2024-03-16 21:23:43 -0300

    metodo concat

---
+++ array01.js
@@ -0,0 +1,6 @@
+// Método concat()
+//Retorna um novo array que é a concatenação do array 'array1' com o array 'array2'.
+let array1 = ['1', '2', '3'];
+let array2 = ['4', '5', '6'];
+let arrayConcatenado = array1.concat(array2);
+console.log(arrayConcatenado); // Saída: ['1', '2', '3', '4', '5', '6']
